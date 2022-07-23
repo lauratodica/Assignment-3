2a51d978e31c33013b5f1581fd81e3eb3acc6768
Laura Todica <laura.todica@gmail.com>
2022-07-23 11:15:46 +0300

    added JS

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const scorePlayer = document.querySelector('.score-p');
+const scoreComputer = document.querySelector('.score-c');
+const winner = document.querySelector('.winner-name');
+const restart = document.querySelector('.start-again');
+
+// console.log(winner.innerText, "winner");
+
+// console.log(scorePlayer.innerText, "PLAYER Score");
+// console.log(scoreComputer.innerText, "COMPUTER score");
+
+// console.log(rock, 'rock');
+// console.log(paper, 'paper');
+// console.log(scissors, 'scissors');
+
+const resetScore = () => {
+    winner.innerText = ""
+    computerWins = 0;
+    playerWins = 0;
+    computerSelection = computerPlay();
+    scorePlayer.innerText = `${playerSelection}: ${playerWins}`;
+    scoreComputer.innerText = `${computerSelection}: ${computerWins}`;
+
+}
+
+restart.addEventListener('click', e => {
+    resetScore();
+})
+
+rock.addEventListener('click', e => {
+    // playRound();
+    playerSelection = (e.target.value = "rock");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    scorePlayer.innerText = `${playerSelection}: ${playerWins}`;
+    scoreComputer.innerText = `${computerSelection}: ${computerWins}`;
+    winnerTime();
+})
+
+paper.addEventListener('click', e => {
+    playerSelection = (e.target.value = "paper");
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    scorePlayer.innerText = `${playerSelection}: ${playerWins}`;
+    scoreComputer.innerText = `${computerSelection}: ${computerWins}`;
+    winnerTime();
+})
+
+scissors.addEventListener('click', e => {
+    playerSelection = (e.target.value = "scissors");
+    // console.log(playerSelection, 'playerSelection-SCISSORS');
+
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+
+    scorePlayer.innerText = `${playerSelection}: ${playerWins}`;
+    scoreComputer.innerText = `${computerSelection}: ${computerWins}`;
+    winnerTime();
+})
+
+
+
+
 function computerPlay() {
     const str = ["rock", "paper", "scissors"];
 
@@ -12,12 +77,14 @@ function playRound(playerSelection, computerSelection) {
     switch(true) {
         case (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") :
             computerWins++;
-            console.log("You lose! Paper beats rock!") 
+            // console.log("You lose! Paper beats rock!") 
+            console.log(computerWins, "computer Wins")
             return computerWins;
             break;
         case (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") :
             playerWins++;
-            console.log("You win! Rock beats scrissors!");
+            console.log(playerWins, "player Wins");
+            // console.log("You win! Rock beats scrissors!");
             return playerWins;
             break;
         case (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock") :
@@ -25,12 +92,14 @@ function playRound(playerSelection, computerSelection) {
             break;
         case (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") :
             playerWins++;
-            console.log("You win! Paper beats rock!");
+            console.log(playerWins, "player Wins");
+            // console.log("You win! Paper beats rock!");
             return playerWins; 
             break;
         case (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors") :
             computerWins++;
-            console.log("You lose! Scissors beats paper!");
+            // console.log("You lose! Scissors beats paper!");
+            console.log(computerWins, "computer Wins")
             return computerWins;
             break;
         case (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper") :
@@ -38,12 +107,14 @@ function playRound(playerSelection, computerSelection) {
             break;
         case (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") :
             computerWins++;
-            console.log("You lose! Rock beats scissors");
+            // console.log("You lose! Rock beats scissors");
+            console.log(computerWins, "computer Wins")
             return computerWins;
             break;
         case (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") :
             playerWins++;
-            console.log("You win! Scissors beats paper");
+            // console.log("You win! Scissors beats paper");
+            console.log(playerWins, "player Wins");
             return playerWins;
             break;
         case (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors") :
@@ -54,24 +125,51 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-function game() {
-    for (let i=0; i<5; i++) {
-        const playerSelection = prompt("Enter your selection - rock, paper or scissors");
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(playerWins, "player");
-        console.log(computerWins, "computer")
-    }
-    switch(true) {
-        case (playerWins > computerWins) :
-            window.prompt("Player is the winner!");
-            break;
-        case (playerWins == computerWins) :
-            window.prompt("It's a draw");
-            break;
-        case (playerWins < computerWins) :
-            window.prompt("Computer is the winner!");
-    }
+const winnerTime = () => {
+    
+    if(playerWins == 5) {
+        console.log(playerWins, 'Player WINS SCORE');
+        winner.innerText = "PLAYER";
+    } else if(computerWins == 5) {
+        winner.innerText = "COMPUTER";    
+    } else {
+        winner.innerText = "";
+    }23
 }
 
-game();
+
+
+// function game() {
+//     for (let i=0; i<5; i++) {
+//         // const playerSelection = "";
+//         const computerSelection = computerPlay();
+//         console.log(playRound(playerSelection, computerSelection));
+//         console.log(playerSelection, "playerS")
+//         console.log(computerSelection, "computerS")
+//         console.log(playerWins, "player");
+//         console.log(computerWins, "computer")
+//     }
+//     switch(true) {
+//         case (playerWins > computerWins) :
+//             console.log("Player is the winner!");
+//             break;
+//         case (playerWins == computerWins) :
+//             console.log("It's a draw");
+//             break;
+//         case (playerWins < computerWins) :
+//             console.log("Computer is the winner!");
+//     }
+// }
+
+// game();
+
+// const oneRound = () => {
+//     const computerSelection = computerPlay();
+
+    // console.log(playRound(playerSelection, computerSelection));
+    //     console.log(playerSelection, "playerS")
+    //     console.log(computerSelection, "computerS")
+    //     console.log(playerWins, "player");
+    //     console.log(computerWins, "computer")
+
+// }
